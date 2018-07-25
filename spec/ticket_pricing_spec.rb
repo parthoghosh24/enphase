b61c8d53d30b2bf2ab1b2a47cf3fbf2761239f6a
Partho Ghosh <partho.ghosh24@gmail.com>
2018-07-25 22:28:23 +0530

    Red Test case added for the problem

--- spec/ticket_pricing_spec.rb
+++ spec/ticket_pricing_spec.rb
@@ -2,7 +2,9 @@ require_relative "../ticket_pricing.rb"
 describe TicketPricing do
 	let(:arr){ [1,2,4,5,7,29,30] }
 	context "testing the solution" do
-		
+		it "returns 11 as minimum price" do
+			expect(TicketPricing.solution(arr)).to eql(11)  
+		end
 	end
 
 end
