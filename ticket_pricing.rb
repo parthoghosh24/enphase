36d476cf7679f63da3cd0af96ec903e8130f64fd
Partho Ghosh <partho.ghosh24@gmail.com>
2018-07-26 00:43:33 +0530

    Test goes green

--- ticket_pricing.rb
+++ ticket_pricing.rb
@@ -9,7 +9,7 @@ class TicketPricing
 		is_trip_taken_on_day = []
 
 		# initializing  is_trip_taken_on_day
-		0..30.each do |day|
+		(0..30).each do |day|
 			is_trip_taken_on_day[day] = false
 		end
 
@@ -20,6 +20,31 @@ class TicketPricing
 
 		# Using tabulation to calculate minimum/optimal cost up to some day
 		min_cost_upto_day =[] 
+
+		min_cost_upto_day[0] = 0 # Just to initialize the table. Day 0 doesnt mean anything
+
+
+		(1..30).each do |day|
+			if !is_trip_taken_on_day[day]
+				min_cost_upto_day[day] = min_cost_upto_day[day-1]
+				next
+			end
+
+			# Option 1 : optimal cost for "day" with one_day_cost
+			min_cost = 	min_cost_upto_day[day-1] + one_day_cost
+
+			# Option 2 : optimal cost for on or ending on "day" with seven_day_cost
+			prev_day = [0,day-7].max
+			while prev_day <= (day - 4) 
+				min_cost = [min_cost, min_cost_upto_day[prev_day]+seven_day_cost].min #optimal cost between min cost or 7 days cost
+				prev_day+= 1
+			end
+
+			# Option 3 : optimal cost for thirty_day_cost
+			min_cost = [min_cost,thirty_day_cost].min
+
+			min_cost_upto_day[day] = min_cost
+		end
 
 		min_cost_upto_day[30]
 
